9ff8c4b22289e95c57e1e5f9792a5bf292a90538
Maria Reccoppa <83091797+mariareccoppa@users.noreply.github.com>
2021-10-18 23:52:23 +0000

    fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let enteredWords = prompt("Please type two words separated with a space");
-let spacePosition = enteredWords .indexOf(" ")
-let firstWord = enteredWords .substring(0, 1 + spacePosition)
-let lastWord = enteredWords .substring(spacePosition, enteredWords.length)
-let firstLetter = firstWord .charAt(0)
-let lastLetter = firstWord .substring(spacePosition - 1)
-let finalfirstWord = firstWord .substring(1, spacePosition-1) 
-console.log (lastLetter+finalfirstWord+firstLetter+lastWord)
+let words = prompt("Please type two words separated with a space");
+let spacePosition = words.indexOf(" ");
+let firstWord = words.substring(1, spacePosition)
+let secondWord = words.substring(spacePosition+1, words.length)
+let firstLetter = words.charAt(0)
+let lastLetter = words.charAt(spacePosition-1)
+
+console.log(lastLetter,firstWord,firstLetter,secondWord)
